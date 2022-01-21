9d38158ae658fe8fe8397072f5b65b67803e2f0f
reynoldadade <reynoldadade@gmail.com>
2022-01-21 11:19:18 +0000

    receive and store products from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductList from "./UI/ProductList";
 
 function App() {
+  //products state
+  const [products, setProducts] = useState([]);
+  /// for loading
+  const [isLoading, setIsLoading] = useState(true);
+
+  //get products from the api
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(
+        "http://www.mocky.io/v2/5c3e15e63500006e003e9795"
+      );
+
+      return response.data.products;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function callAPI() {
+      const products = await getProducts();
+      setIsLoading(false);
+      setProducts(products);
+    }
+
+    callAPI();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading && <p>Loading ...</p>}
+      {!isLoading && products.length > 0 && <ProductList products={products} />}
+      {!isLoading && products.length === 0 && <p>No products found</p>}
     </div>
   );
 }
