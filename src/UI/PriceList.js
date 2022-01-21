7bb1ee27f38df41015253cc6f9c6f55900da1008
reynoldadade <reynoldadade@gmail.com>
2022-01-21 12:55:33 +0000

    install redux

---
+++ src/UI/PriceList.js
@@ -0,0 +1,3 @@
+const PriceList = () => {
+  return <div></div>;
+};
--- src/UI/ProductItem.js
+++ src/UI/ProductItem.js
@@ -1,7 +1,12 @@
 //create ProductListItem
 
 const ProductList = ({ product }) => {
-  return <div className="product-list">{product.name}</div>;
+  return (
+    <div className="p-4 border border-black flex">
+      <div>{product.name}</div>
+      {/* <div>{product.prices}</div> */}
+    </div>
+  );
 };
 
 export default ProductList;
